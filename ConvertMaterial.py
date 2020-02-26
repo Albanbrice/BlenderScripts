7c6da2ea32c9ab2e8f7b64b3cdff92d2a4462cde
archeo3d <bricea75@hotmail.com>
2020-02-26 15:26:13 +0100

    Create ConvertMaterial.py

---
+++ ConvertMaterial.py
@@ -0,0 +1,16 @@
+# Convert Principled to Emission
+
+import bpy
+
+materials = bpy.data.materials
+
+for mat in materials:
+    if mat.node_tree.nodes.get('Principled BSDF'):
+        mat.node_tree.nodes.remove(mat.node_tree.nodes.get('Principled BSDF'))
+        mat_output = mat.node_tree.nodes.get('Material Output')
+        emission = mat.node_tree.nodes.new('ShaderNodeEmission')
+        texture = mat.node_tree.nodes.get('Image Texture')    
+        mat.node_tree.links.new(mat_output.inputs[0], emission.outputs[0])
+        mat.node_tree.links.new(emission.inputs[0], texture.outputs[0])             
+    else:
+        break
